12b51fe5e73efa1e430911908b9c6f98dc113172
Torres456 <barrientostorres9@gmail.com>
2024-07-19 12:46:37 -0600

    add login

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+])->group(function () {
+    Route::get('/administrador', function () {
         return view('dashboard');
     })->name('dashboard');
 });
