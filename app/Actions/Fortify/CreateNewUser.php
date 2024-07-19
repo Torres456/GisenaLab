f0320b7f9698be2aba039e3faf600e8b5b8a6575
Torres456 <barrientostorres9@gmail.com>
2024-07-19 05:27:27 -0600

    add new parameters for user registration

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -3,10 +3,12 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+use App\Models\cliente;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\DB;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -19,17 +21,50 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+
+        $messages = [
+            'rfc.max' => 'El campo RFC no debe ser mayor que 15 caracteres.',
+            'rfc.min' => 'El campo RFC debe contener al menos 12 caracteres.',
+            'rfc.unique' => 'El campo RFC ya ha sido registrado.',
+            'nombre.max' => 'El campo Nombre, denominación o razón social no debe ser mayor que 255 caracteres.',
+            'nombre.required' => 'El campo Nombre, denominación o razón social es obligatorio.',
+        ];
+
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'nombre' => ['required', 'string', 'max:255'],
+            'rfc' => ['required', 'string', 'max:15', 'min:12', 'unique:cliente'],
+            'correo' => ['required', 'string', 'email', 'max:255', 'unique:usuario_sistema'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ])->validate();
+        ], $messages)->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+
+        DB::beginTransaction();
+        try {
+            $usuario_sistema = User::create([
+                'correo' => $input['correo'],
+                'contraseña' => Hash::make($input['password']),
+                'estatus' => 1,
+                'idtipo_usuario' => 2
+            ]);
+
+            $id = DB::table('usuario_sistema')->where('correo', $input['correo'])->value('idusuario_sistema');
+
+            $cliente = cliente::create([
+                'razon_social' => $input['nombre'],
+                'rfc' => $input['rfc'],
+                'tipo' => 1,
+                'correo' => $input['correo'],
+                'idusuario_sistema' => $id
+            ]);
+
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return $e->getMessage();
+        }
+
+        return $usuario_sistema;
     }
 }
