943faafbd85d6cd97898839d495bdd93b1781f9e
Torres456 <barrientostorres9@gmail.com>
2024-07-19 01:07:04 -0600

    create model colonia

---
+++ app/Models/colonia.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class colonia extends Model
+{
+    use HasFactory;
+    protected $table = 'colonia';
+    protected $primaryKey = 'id_colonia';
+
+    protected $fillable = [
+        'nombre',
+        'clave_colonia',
+        'id_municipio'
+    ];
+
+
+    public function municipio(): BelongsTo
+    {
+        return $this->belongsTo(municipio::class, 'id_municipio');
+    }
+
+
+}
