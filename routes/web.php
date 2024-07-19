5ae4683a7c24ac8212ca5f8dabaa91bd312cfe15
LuisLuna24 <eduarlun4@gmail.com>
2024-07-19 13:08:07 -0600

    rutas catalogos

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,66 @@ Route::middleware([
     Route::get('/administrador', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/catalogos', function () {
+        return view('catalogos.index');
+    })->name('catalogos.index');
+
+
+    Route::get('/catalogo/categorias', function () {
+        return view('catalogos.categorias');
+    })->name('catalogos.categorias');
+
+    Route::get('/catalogo/subcategorias', function () {
+        return view('catalogos.subcategorias');
+    })->name('catalogos.subcategorias');
+
+    Route::get('/catalogo/tipo_muetras', function () {
+        return view('catalogos.tipo_muestras');
+    })->name('catalogos.tipo_muestras');
+
+    Route::get('/catalogo/tipo_analisis', function () {
+        return view('catalogos.tipo_analisis');
+    })->name('catalogos.tipo_analisis');
+
+    Route::get('/catalogo/analisi_esoecificos', function () {
+        return view('catalogos.analisis_especifico');
+    })->name('catalogos.analisis_especifico');
+
+    Route::get('/catalogo/metodos', function () {
+        return view('catalogos.metodos');
+    })->name('catalogos.metodos');
+
+    Route::get('/catalogo/unidad_metodo', function () {
+        return view('catalogos.unidad_metodo');
+    })->name('catalogos.unidad_metodo');
+
+    Route::get('/catalogo/unidad_medidas', function () {
+        return view('catalogos.unidad_medida');
+    })->name('catalogos.unidad_medida');
+
+    Route::get('/catalogo/contenedores', function () {
+        return view('catalogos.contenedores');
+    })->name('catalogos.contenedores');
+
+    Route::get('/catalogo/recipientes', function () {
+        return view('catalogos.recipientes');
+    })->name('catalogos.recipientes');
+
+    Route::get('/catalogo/estatus_orden_servicio', function () {
+        return view('catalogos.status_orden_servicio');
+    })->name('catalogos.status_orden_servicio');
+
+    Route::get('/catalogo/estatus_muestra', function () {
+        return view('catalogos.status_muestra');
+    })->name('catalogos.status_muestra');
+
+
+    Route::get('/catalogo/rutas', function () {
+        if (auth()->user()->email == 'lued1006@gmail.com'){
+            return view('rutas.index');
+        }else{
+            abort(404);
+        }
+    })->name('rutas.index');
 });
