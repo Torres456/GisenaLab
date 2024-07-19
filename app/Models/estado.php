7dc5a6d23e916116e4aced74bd4127603078b6fb
Torres456 <barrientostorres9@gmail.com>
2024-07-19 01:08:19 -0600

    create model estado

---
+++ app/Models/estado.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class estado extends Model
+{
+    use HasFactory;
+    protected $table = 'estado';
+    protected $primaryKey = 'id_estado';
+
+    protected $fillable = [
+        'nombre',
+        'clave_estado'
+    ];
+
+    //One to Many
+    public function municipio(): HasMany
+    {
+        return $this->hasMany(municipio::class,'id_estado');
+    }
+    
+    // //Zona representacion
+    // public function zona(): BelongsToMany
+    // {
+    //     return $this->belongsToMany(zona_representacion::class);
+    // }
+
+    // //direccion estado
+    // public function direccion_estado(): HasMany
+    // {
+    //     return $this->hasMany(direccion::class);
+    // }
+
+    // //direccion municipio
+    // public function direccion_municipio(): HasMany
+    // {
+    //     return $this->hasMany(municipio::class);
+    // }
+
+    //  //direccion colona
+    //  public function direccion_colonia(): HasMany
+    //  {
+    //      return $this->hasMany(colonia::class);
+    //  }
+     
+}
