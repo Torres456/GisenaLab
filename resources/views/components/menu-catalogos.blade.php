116a5102b569fd0f932bab66583a6384fa3f3f64
LuisLuna24 <eduarlun4@gmail.com>
2024-07-30 16:17:28 -0600

    New components

--- resources/views/components/menu-catalogos.blade.php
+++ resources/views/components/menu-catalogos.blade.php
@@ -1,5 +1,5 @@
 <div class="flex gap-4 justify-center items-center">
-    <x-dropdown>
+    <x-dropdown-catalogos>
         <x-slot name="trigger">
             <div class="flex dark:text-white justify-center items-center gap-2 cursor-pointer">
                 <p>Catálogos</p> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -15,6 +15,6 @@
         <x-slot name="content">
             @livewire('componentes.catalogos')
         </x-slot>
-    </x-dropdown>
+    </x-dropdown-catalogos>
     <x-button-return href="{{ route('catalogos.index') }}" wire:navigate.hover>Regresar</x-button-return>
 </div>
---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,53 @@
+<div>
+    @if (session('green'))
+        <div class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800"
+            role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-check">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+                <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+                <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+                <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+                <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+                <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+                <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+                <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+                <path d="M9 12l2 2l4 -4" />
+            </svg>
+            <div class="ml-2">
+                {{ session('green') }}
+            </div>
+        </div>
+    @elseif(session('red'))
+        <div class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+            role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-square-x">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
+                <path d="M9 9l6 6m0 -6l-6 6" />
+            </svg>
+            <div class="ml-2">
+                {{ session('red') }}
+            </div>
+        </div>
+    @elseif(session('blue'))
+        <div class="flex items-center p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+            role="alert">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-edit-circle">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z" />
+                <path d="M16 5l3 3" />
+                <path d="M9 7.07a7 7 0 0 0 1 13.93a7 7 0 0 0 6.929 -6" />
+            </svg>
+            <div class="ml-2">
+                {{ session('blue') }}
+            </div>
+        </div>
+    @endif
+</div>
