ffb0b4912a2999ddfa1578659a672367cb7d4b36
Torres456 <barrientostorres9@gmail.com>
2024-08-06 16:08:51 -0600

    create rules folder

---
+++ app/Rules/Fisica.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Fisica implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $primerosCaracteres = substr($value, 0, 4);
+
+        $SegundosCaracteres = substr($value, 5, 5);
+
+
+        if ($value != strtoupper($value)) {
+            $fail('El campo RFC debe contener puras mayúsculas.');
+        }
+
+        if (preg_match('/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/u', $primerosCaracteres)) {
+        } else {
+            $fail('Campo RFC incorrecto.');
+        }
+
+        if (is_numeric($SegundosCaracteres)) {
+        } else {
+            $fail('Campo RFC incorrecto.');
+        }
+    }
+}
---
+++ app/Rules/Moral.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Moral implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $primerosCaracteres = substr($value, 0, 2);
+
+        $SegundosCaracteres = substr($value, 3, 6);
+
+
+        if ($value != strtoupper($value)) {
+            $fail('El campo RFC debe contener puras mayúsculas.');
+        }
+
+        if (preg_match('/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/u', $primerosCaracteres)) {
+        } else {
+            $fail('Campo RFC incorrecto.');
+        }
+
+        if (is_numeric($SegundosCaracteres)) {
+        } else {
+            $fail('Campo RFC incorrecto.');
+        }
+    }
+}
