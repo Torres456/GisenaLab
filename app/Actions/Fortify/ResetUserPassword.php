5f11380c1c2b03bf8a7956deb8c69782522d7171
Torres456 <barrientostorres9@gmail.com>
2024-08-07 20:13:12 -0600

    change variable, password to contraseña of forceFill

--- app/Actions/Fortify/ResetUserPassword.php
+++ app/Actions/Fortify/ResetUserPassword.php
@@ -23,7 +23,7 @@ class ResetUserPassword implements ResetsUserPasswords
         ])->validate();
 
         $user->forceFill([
-            'password' => Hash::make($input['password']),
+            'contraseña' => Hash::make($input['password']),
         ])->save();
     }
 }
