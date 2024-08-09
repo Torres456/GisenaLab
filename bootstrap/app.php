28eea745fd33f620832b7930f1b8bd91eee11ce5
Torres456 <barrientostorres9@gmail.com>
2024-08-09 12:04:21 -0600

    add group routes whit middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,45 @@
 <?php
 
+use App\Http\Middleware\EnsureUserHasRol;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware([
+                'web',
+                'auth:sanctum',
+                'role:1',
+                config('jetstream.auth_session'),
+            ])
+                ->prefix('admin')
+                ->name('admin.')
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware([
+                'web',
+                'auth:sanctum',
+                'role:2',
+                'verified',
+                config('jetstream.auth_session'),
+            ])
+                ->prefix('cliente')
+                ->name('cliente.')
+                ->group(base_path('routes/client.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        $middleware->alias([
+            'role' => EnsureUserHasRol::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
