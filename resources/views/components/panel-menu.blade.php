bfb2c6da5466d731ba4a66e97f8795f3ffb5a197
LuisLuna24 <eduarlun4@gmail.com>
2024-08-12 09:57:38 -0600

    end

--- resources/views/components/panel-menu.blade.php
+++ resources/views/components/panel-menu.blade.php
@@ -206,7 +206,7 @@
                         <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
                         <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
                     </svg>
-                    <span class="ms-3">{{ __('Catalogos') }}</span>
+                    <span class="ms-3">{{ __('Órdenes Servicio') }}</span>
                 </x-nav-link>
             </li>
             <li>
