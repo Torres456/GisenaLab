144a89c3f65b3e1a563e0b874d26726792f57df5
Torres456 <barrientostorres9@gmail.com>
2024-08-15 12:40:57 -0600

    add new fields for user registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,33 +7,60 @@
             <x-authentication-card-logo />
         </x-slot>
 
-        <x-validation-errors class="mb-4" />
-
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div>
-                <x-label for="nombre" value="{{ __('Nombre, denominación o razón social') }}" />
-                <x-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" required
-                    autofocus autocomplete="nombre" />
+                <x-label for="nombre" value="{{ __('Nombre') }}" />
+                <x-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" autofocus
+                    autocomplete="nombre"
+                    onkeypress="return (event.charCode >= 65 && event.charCode <= 122 || event.charCode >= 32 && event.charCode <= 32 || event.charCode >= 225 && event.charCode <= 225 || event.charCode >= 233 && event.charCode <= 237 || event.charCode >= 243 && event.charCode <= 250 || event.charCode >= 192 && event.charCode <= 218 || event.charCode >= 209 && event.charCode <= 209 || event.charCode >= 241 && event.charCode <= 241)"
+                    maxlength=255 />
+                <x-input-error for="nombre" class="mt-2" />
             </div>
 
             <div class="mt-4">
-                <x-label for="rfc" value="{{ __('RFC') }}" />
-                <x-input id="rfc" class="block mt-1 w-full" type="text" name="rfc" :value="old('rfc')"
-                    required autofocus autocomplete="rfc" />
+                <x-label for="paterno" value="{{ __('Apellido paterno') }}" />
+                <x-input id="paterno" class="block mt-1 w-full" type="text" name="paterno" :value="old('paterno')"
+                    required autofocus autocomplete="apellido paterno" max="255"
+                    onkeypress="return (event.charCode >= 65 && event.charCode <= 122 || event.charCode >= 32 && event.charCode <= 32 || event.charCode >= 225 && event.charCode <= 225 || event.charCode >= 233 && event.charCode <= 237 || event.charCode >= 243 && event.charCode <= 250 || event.charCode >= 192 && event.charCode <= 218 || event.charCode >= 209 && event.charCode <= 209 || event.charCode >= 241 && event.charCode <= 241)"
+                    maxlength=255 />
+                <x-input-error for="paterno" class="mt-2" />
             </div>
+
+            <div class="mt-4">
+                <x-label for="materno" value="{{ __('Apellido materno') }}" />
+                <x-input id="materno" class="block mt-1 w-full" type="text" name="materno" :value="old('materno')"
+                    required autofocus autocomplete="apellido materno" min="255"
+                    onkeypress="return (event.charCode >= 65 && event.charCode <= 122 || event.charCode >= 32 && event.charCode <= 32 || event.charCode >= 225 && event.charCode <= 225 || event.charCode >= 233 && event.charCode <= 237 || event.charCode >= 243 && event.charCode <= 250 || event.charCode >= 192 && event.charCode <= 218 || event.charCode >= 209 && event.charCode <= 209 || event.charCode >= 241 && event.charCode <= 241)"
+                    maxlength=255 />
+                <x-input-error for="materno" class="mt-2" />
+            </div>
+
+            <div class="mt-4">
+                <x-label-tooltip value="{{ __('RFC') }}"
+                    message-text="El RFC debe tener como minimo 13 caracteres para persona fisica y 12 para persona moral." />
+                <x-input id="rfc" class="block mt-1 w-full" type="text" name="rfc" :value="old('rfc')"
+                    required autofocus autocomplete="rfc"
+                    onkeypress="return (event.charCode >= 65 && event.charCode <=90 || event.charCode >= 97 && eve
+                    nt.charCode <=122 )"
+                    onkeyup="javascript:this.value = this.value.toUpperCase();" maxlength=13 />
+                <x-input-error for="rfc" class="mt-2" />
+            </div>
+
 
             <div class="mt-4">
                 <x-label for="correo" value="{{ __(' Correo') }}" />
                 <x-input id="correo" class="block mt-1 w-full" type="email" name="correo" :value="old('correo')"
                     required autocomplete="username" />
+                <x-input-error for="correo" class="mt-2" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Contraseña') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="new-password" />
+                <x-input-error for="password" class="mt-2" />
             </div>
 
             <div class="mt-4">
