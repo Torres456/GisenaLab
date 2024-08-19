e5511390c505e2812696dc7e3d824325cdb41c0d
LuisLuna24 <eduarlun4@gmail.com>
2024-08-19 17:46:01 -0600

    Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,42 @@ class AppServiceProvider extends ServiceProvider
 
     public function register(): void
     {
+        $this->app->instance(RegisterResponse::class, new class implements RegisterResponse
+        {
+            public function toResponse($request)
+            {
+
+                return redirect('/dashboard');
+            }
+        });
+
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse
+        {
+            public function toResponse($request)
+            {
+
+
+
+                if (Auth::check()) {
+
+                    $user = Auth::user();
+
+
+                    if ($user->idtipo_usuario == 1) {
+
+                        return redirect('/administrador');
+                    } else if ($user->idtipo_usuario == 2) {
+
+                        return redirect()->route('client.panel');
+                    } else if ($user->idtipo_usuario == '3') {
+
+                        return redirect()->route('gestor.panel');
+                    }
+                } else {
+                    return redirect('/login');
+                }
+            }
+        });
     }
 
     /**
