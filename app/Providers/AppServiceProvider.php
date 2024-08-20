a315f37e7f5e3eb31b5de82bd745bb2bdda6534d
Torres456 <barrientostorres9@gmail.com>
2024-08-20 14:02:42 -0600

    delete routes register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,45 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class AppServiceProvider extends ServiceProvider
 {
 
-    public function register(): void
-    {
-        $this->app->instance(RegisterResponse::class, new class implements RegisterResponse
-        {
-            public function toResponse($request)
-            {
-
-                return redirect('/dashboard');
-            }
-        });
-
-        $this->app->instance(LoginResponse::class, new class implements LoginResponse
-        {
-            public function toResponse($request)
-            {
-
-
-
-                if (Auth::check()) {
-
-                    $user = Auth::user();
-
-
-                    if ($user->idtipo_usuario == 1) {
-
-                        return redirect('/administrador');
-                    } else if ($user->idtipo_usuario == 2) {
-
-                        return redirect()->route('client.panel');
-                    } else if ($user->idtipo_usuario == '3') {
-
-                        return redirect()->route('gestor.panel');
-                    }
-                } else {
-                    return redirect('/login');
-                }
-            }
-        });
-    }
+    public function register(): void {}
 
     /**
      * Bootstrap any application services.
