3a5bd7e9ac9c7aea551878d4fcacc6c9182dcf88
Torres456 <barrientostorres9@gmail.com>
2024-08-30 16:00:06 -0600

    add layout

--- resources/views/interesado/panel.blade.php
+++ resources/views/interesado/panel.blade.php
@@ -1,11 +1,4 @@
-<x-app-layout>
-    <x-panel-menu>
-        <x-slot name="content">
-            <div class="grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-5">
-                <h2 class="dark:text-teal-50">Panel del interesado</h2>
-            </div>
-        </x-slot>
-    </x-panel-menu>
-</x-app-layout>
-
-
+<x-guest-layout>
+    <x-header />
+    <h1>Panel de interesado</h1>
+</x-guest-layout>
