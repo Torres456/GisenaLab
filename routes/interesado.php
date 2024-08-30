3b984561d3b44ed809b7deb6c725de8e4b403a5f
Torres456 <barrientostorres9@gmail.com>
2024-08-30 16:02:01 -0600

    change name rute

--- routes/interesado.php
+++ routes/interesado.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/panel', function () {
+Route::get('/perfil', function () {
     return view('interesado.panel');
-})->name('panel'); 
+})->name('interesado.perfil');
