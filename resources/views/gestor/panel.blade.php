16baa9fe9cc18b7628e667638540dcb96ee42448
Torres456 <barrientostorres9@gmail.com>
2024-08-30 15:59:29 -0600

    add layout

--- resources/views/gestor/panel.blade.php
+++ resources/views/gestor/panel.blade.php
@@ -1,11 +1,10 @@
 <x-app-layout>
-    <x-panel-menu>
+    {{-- <x-panel-menu>
         <x-slot name="content">
             <div class="grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-5">
                 <h2 class="dark:text-teal-50">Panel del gestor</h2>
             </div>
         </x-slot>
-    </x-panel-menu>
+    </x-panel-menu> --}}
+    <h1>Panel de gestor</h1>
 </x-app-layout>
-
-
