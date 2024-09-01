b86c27d0258cea0e5fd63df16cdffb068505dee9
Torres456 <barrientostorres9@gmail.com>
2024-09-01 14:22:46 -0600

    add placeholders

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
             <div>
                 <x-label for="nombre" value="{{ __('Nombre') }}" />
                 <x-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" autofocus
-                    autocomplete="nombre"
+                    placeholder="Introduce tu nombre" autocomplete="nombre"
                     onkeypress="return (event.charCode >= 65 && event.charCode <= 122 || event.charCode >= 32 && event.charCode <= 32 || event.charCode >= 225 && event.charCode <= 225 || event.charCode >= 233 && event.charCode <= 237 || event.charCode >= 243 && event.charCode <= 250 || event.charCode >= 192 && event.charCode <= 218 || event.charCode >= 209 && event.charCode <= 209 || event.charCode >= 241 && event.charCode <= 241)"
                     maxlength=255 />
                 <x-input-error for="nombre" class="mt-2" />
@@ -23,6 +23,7 @@
                 <x-label for="paterno" value="{{ __('Apellido paterno') }}" />
                 <x-input id="paterno" class="block mt-1 w-full" type="text" name="paterno" :value="old('paterno')"
                     required autofocus autocomplete="apellido paterno" max="255"
+                    placeholder="Introduce tu apellido paterno"
                     onkeypress="return (event.charCode >= 65 && event.charCode <= 122 || event.charCode >= 32 && event.charCode <= 32 || event.charCode >= 225 && event.charCode <= 225 || event.charCode >= 233 && event.charCode <= 237 || event.charCode >= 243 && event.charCode <= 250 || event.charCode >= 192 && event.charCode <= 218 || event.charCode >= 209 && event.charCode <= 209 || event.charCode >= 241 && event.charCode <= 241)"
                     maxlength=255 />
                 <x-input-error for="paterno" class="mt-2" />
@@ -32,6 +33,7 @@
                 <x-label for="materno" value="{{ __('Apellido materno') }}" />
                 <x-input id="materno" class="block mt-1 w-full" type="text" name="materno" :value="old('materno')"
                     required autofocus autocomplete="apellido materno" min="255"
+                    placeholder="Introduce tu apellido materno"
                     onkeypress="return (event.charCode >= 65 && event.charCode <= 122 || event.charCode >= 32 && event.charCode <= 32 || event.charCode >= 225 && event.charCode <= 225 || event.charCode >= 233 && event.charCode <= 237 || event.charCode >= 243 && event.charCode <= 250 || event.charCode >= 192 && event.charCode <= 218 || event.charCode >= 209 && event.charCode <= 209 || event.charCode >= 241 && event.charCode <= 241)"
                     maxlength=255 />
                 <x-input-error for="materno" class="mt-2" />
@@ -41,7 +43,7 @@
                 <x-label-tooltip value="{{ __('RFC') }}"
                     message-text="El RFC debe tener como minimo 13 caracteres para persona fisica y 12 para persona moral." />
                 <x-input id="rfc" class="block mt-1 w-full" type="text" name="rfc" :value="old('rfc')"
-                    required autofocus autocomplete="rfc"
+                    required autofocus autocomplete="rfc" placeholder="Ejemplo: ABCD123456XYZ"
                     onkeypress="return (event.charCode >= 65 && event.charCode <=90 || event.charCode >= 97 && eve
                     nt.charCode <=122 )"
                     onkeyup="javascript:this.value = this.value.toUpperCase();" maxlength=13 />
@@ -52,7 +54,7 @@
             <div class="mt-4">
                 <x-label for="correo" value="{{ __(' Correo') }}" />
                 <x-input id="correo" class="block mt-1 w-full" type="email" name="correo" :value="old('correo')"
-                    required autocomplete="username" />
+                    required autocomplete="username" placeholder="nombre@dominio.com" />
                 <x-input-error for="correo" class="mt-2" />
             </div>
 
