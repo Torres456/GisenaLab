d877fca24c7616389fde6f3c5e9d579ae45f8a51
Torres456 <barrientostorres9@gmail.com>
2024-09-18 00:09:37 -0600

    add panels folder

---
+++ app/View/Components/paneles/personal.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\View\Components\paneles;
+
+use App\Models\rutas;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\Auth;
+
+
+
+class personal extends Component
+{
+
+    public $rutas;
+
+    public $titulo;
+
+    public function __construct()
+    {
+
+        switch (Auth::user()->id_tipo_usuario) {
+
+            case 1:
+                $this->rutas = rutas::where('estado', 1)->where('tipo', 1)->get();
+                break;
+
+            case 3:
+                $this->rutas = rutas::where('estado', 1)->where('tipo', 3)->get();
+                break;
+
+            case 5:
+                $this->rutas = rutas::where('estado', 1)->where('tipo', 5)->get();
+                break;
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.paneles.personal');
+    }
+}
