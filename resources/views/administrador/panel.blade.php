788d7f4dd5e3b55d2a1448c483cd5aa62a8b72a1
Torres456 <barrientostorres9@gmail.com>
2024-09-29 15:08:32 -0600

    update dark mode

--- resources/views/administrador/panel.blade.php
+++ resources/views/administrador/panel.blade.php
@@ -2,7 +2,9 @@
     <x-slot:titulo>
         Principal
     </x-slot>
-    <div class="font-semibold text-lg text-black dark:text-gray-200 leading-tight">
+    <div class="font-semibold text-lg text-black dark:text-gray-200 leading-tight  text-end mb-3">
         <h2>¡Hola, {{ Auth::user()->nombre }}!</h2>
     </div>
+    @livewire('administrador.componentes.principal.contenedor-cartas')
+
 </x-paneles.personal>
