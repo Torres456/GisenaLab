5a62e667975022860133520254e923132f67dec6
Torres456 <barrientostorres9@gmail.com>
2024-09-29 15:13:15 -0600

    add new file components

---
+++ app/Livewire/Administrador/Componentes/Principal/ContenedorCartas.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Livewire\Administrador\Componentes\Principal;
+
+use App\Models\cliente;
+use App\Models\empleado;
+use App\Models\gestor;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class ContenedorCartas extends Component
+{
+
+    public $usuarios;
+
+    public $clientes;
+
+    public $clientes_gestor;
+
+    public $gestores;
+
+    public $empleados;
+
+
+    public function mount()
+    {
+        $this->usuarios = User::where('id_tipo_usuario', '2')->count();
+
+        $this->clientes = cliente::count();
+
+        $this->clientes_gestor = DB::table('cliente_direccion')->join('cliente', 'cliente_direccion.id_cliente', '=', 'cliente.id_cliente')->where('cliente.id_gestor', NULL)->count();
+
+        $this->gestores = gestor::count();
+
+        $this->empleados = empleado::count();
+    }
+
+
+    public function refresh()
+    {
+
+        $this->usuarios = User::where('id_tipo_usuario', '2')->count();
+
+        $this->clientes = cliente::count();
+
+        $this->clientes_gestor = DB::table('cliente_direccion')->join('cliente', 'cliente_direccion.id_cliente', '=', 'cliente.id_cliente')->where('cliente.id_gestor', NULL)->count();
+
+        $this->gestores = gestor::count();
+
+        $this->empleados = empleado::count();
+    }
+
+
+
+    public function render()
+    {
+        return view('livewire.administrador.componentes.principal.contenedor-cartas');
+    }
+}
